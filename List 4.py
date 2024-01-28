bf3431a3cc3cd439649d162b5ddc79bb4c4dfd44
19JN1AO543Hima <156064049+19JN1AO543Hima@users.noreply.github.com>
2024-01-28 16:00:52 +0530

    Added 4th program of Problem solving task-3

---
+++ List 4.py
@@ -0,0 +1,13 @@
+integer = 2048
+
+integer = str(integer)
+
+first_digit = int(integer[0])
+
+last_digit = int(integer[-1])
+
+addition = first_digit + last_digit
+
+print("Sum of first and last digit of the integer is: ")
+
+print(addition)
